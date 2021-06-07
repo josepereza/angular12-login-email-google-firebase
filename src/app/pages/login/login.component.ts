91a3088fa922f6620c0360d9ce473a5afa183603
josepereza <josepereza66@gmail.com>
2021-06-07 17:47:48 +0200

    cambio login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -10,6 +10,7 @@ export class LoginComponent implements OnInit {
 email:string=''
 password:string=''
 user!:any
+mostraralerta:boolean=false
   constructor(public auth:AngularFireAuth) { }
 
   ngOnInit(): void {
@@ -20,7 +21,13 @@ this.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider ).then(res=>this.
 
 loginEmail(){
   console.log('ostias',this.email,this.password)
-this.auth.signInWithEmailAndPassword(this.email,this.password).then(res=>this.user=res)
+this.auth.signInWithEmailAndPassword(this.email,this.password).then(res=>{
+  this.mostraralerta=false;
+  return this.user=res})
+.catch(error => {
+  this.mostraralerta=true;
+  console.log('Error en el email o password', error);
+});
 }
 logout(){
   this.auth.signOut()
